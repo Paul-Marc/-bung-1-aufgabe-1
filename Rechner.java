ef0fce85447fdf3960c47df98cc630b711e3cbdd
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-25 14:14:03 +0200

    erstellung der datein. sollte auch schon richtig sein. es fehlt nur dokumentation. bei mir gibt es aber einen fehler mit der class version. ka was das ist

---
+++ Rechner.java
@@ -0,0 +1,14 @@
+public class Rechner{
+	
+	public static void main(String[] args){
+		System.out.println(
+		"Taschenrechner \n" +
+		"Was moechten Sie tun? \n" +
+		"  <a> - Addieren \n" +
+		"  <b> - Suptrahieren \n" +
+		"  <c> - Multiplizieren \n" +
+		"  <d> - Dividieren \n" + 
+		"  Abbruch mit jedem anderen Zeichen"
+		);
+	} 
+}
