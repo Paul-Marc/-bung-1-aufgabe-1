e91eb0048e3356d9797ade4dbcf6068c08764f85
Paul Conrad <paulconrad21@gmail.com>
2019-10-27 14:27:33 +0100

    +Plan, +Ziel, +Class, +.java

---
+++ Rechner.java
@@ -0,0 +1,17 @@
+// Blatt 1 Aufgabe 1: Paul Conrad 358940, Marc Palfner 359899 
+
+public class Rechner{
+
+public static void main(String[] args) {
+
+System.out.println("Taschenrechner");
+System.out.println("Was moechten Sie tun?");
+System.out.println("  <a> - Addieren");
+System.out.println("  <b> - Subtrahieren");
+System.out.println("  <c> - Multiplizieren");
+System.out.println("  <d> - Dividieren");
+System.out.println("  Abbruch mit jedem anderen Zeichen.");
+
+    }
+
+}
